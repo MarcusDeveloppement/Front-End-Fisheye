ed829f00a42a6e1d3d56c769ad9a878e8091a920
MarcusDeveloppement <marcus.frontdev@gmail.com>
2023-09-11 15:15:24 +0200

    setting up the header photographer page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,30 @@
-//Mettre le code JavaScript lié à la page photographer.html
+import { photographerInfo } from "../utils/photographeInfoRecup.js";
+const getPhotographerInfo = await photographerInfo();
+
+// create funtion for information header
+function PhotographHeader(data) {
+  const { name, city, country, tagline, portrait } = data;
+
+  const photographHeader = `
+    <section class="photograph-header">
+      <div class="photograph-info">
+        <h1 class="photograph-name">${name}</h1>
+        <p class="photograph-zone">${city}, ${country}</p>
+        <p class="photograph-tagline">${tagline}</p>
+      </div>
+      <button class="contact_button" id="contactBtn" aria-label="Bouton d'ouverture du formulaire de contact">Contactez-moi</button>
+      <img class="photograph-img" src="assets/photographers/${portrait}" alt="Photo de ${name}">
+    </section>
+  `;
+
+  // select element where i display the information
+  const mainElem = document.querySelector("main");
+  mainElem.innerHTML += photographHeader;
+}
+
+// create a function to call and render the result
+async function renderPhotographPage() {
+  await PhotographHeader(getPhotographerInfo);
+}
+
+renderPhotographPage();
---
+++ scripts/utils/photographeInfoRecup.js
@@ -0,0 +1,13 @@
+import { getPhotographers } from "./dataImport.js";
+
+// call data and retrieve id for obtain photographe informations
+export async function photographerInfo() {
+  const { photographers } = await getPhotographers();
+
+  const params = new URL(document.location).searchParams;
+  const photographerId = parseInt(params.get("id"));
+
+  return photographers.find(
+    (photographer) => photographer.id === photographerId
+  );
+}
